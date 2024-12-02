e0c2142fa59cb0c4eae1041bd96c239cdfb24f0f
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-12-02 14:02:43 +0500

    js

---
+++ main.js
@@ -0,0 +1,57 @@
+// 1 ----------------------------------------------------------1
+// let str = 'JavaScriptIsAmazing'
+// let start = 4
+// let end = 10
+
+// let res = str.slice(start, end).toLowerCase();
+
+// console.log(res);
+
+// 2 ---------------------------------------------------------2
+// let str = "  Find spaces here  "
+// let strTrim = str.trim()
+// let res = ''
+
+// for (let i = 0; i < strTrim.length; i++) {
+//     if (strTrim[i] === " ") {
+//         res += i + ' '
+//     }    
+// }
+
+// console.log(res);
+
+
+// 3 -----------------------------------------------------------3
+
+
+// let str = 'apple orange apple banana orange';
+// let strSplit = str.split(" ");
+// let res = ''; 
+
+// for (let i = 0; i < strSplit.length; i++) {
+    
+//     if (!res.includes(strSplit[i])) {
+//         res += strSplit[i] + " ";
+//     }
+// }
+
+// console.log(res.trim().split(" "));
+
+
+
+
+
+
+//  4 ----------------------------------------------------------4
+// let str = 'apple123orange456banana789'
+// let onlyStr = ''
+// let res = ''
+
+// for (let i = 0; i < str.length; i++) {
+//     onlyStr += str[i]
+//     if (isNaN(onlyStr[i])) {
+//         res += onlyStr[i]
+//     }
+// }
+
+// console.log(res);
